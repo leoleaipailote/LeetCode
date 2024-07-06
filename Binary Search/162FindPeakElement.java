d8a65a4f0098e43ddb03b5ee478e1d4154bee1c5
leoleaipailote <136939887+leoleaipailote@users.noreply.github.com>
2024-07-05 23:05:13 -0400

    Find Peak Element

---
+++ Binary Search/162FindPeakElement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1)
+            return 0;
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[nums.length - 1] > nums[nums.length - 2])
+            return nums.length - 1;
+
+        int left = 0;
+        int right = nums.length;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            if (nums[mid] > nums[mid - 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return -1;
+    }
+}
