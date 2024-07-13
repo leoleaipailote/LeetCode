2e4e3614696d0e0cdad9030c701789973750abbf
leoleaipailote <136939887+leoleaipailote@users.noreply.github.com>
2024-07-13 05:18:14 -0400

    maximal square

---
+++ DynamicProgramming/221MaximalSquare.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        int max = 0;
+        for (int i = 0; i < rows; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            max = Math.max(max, dp[i][0]);
+        }
+        for (int i = 0; i < cols; i++) {
+            dp[0][i] = matrix[0][i] - '0';
+            max = Math.max(max, dp[0][i]);
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == '0')
+                    continue;
+                dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max * max;
+    }
+}
